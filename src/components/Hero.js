c77b359669413959bca16f74274cfce7e85a9b80
Lokesh <lokeshsingh7695@gmail.com>
2024-05-12 20:37:10 +0530

    Hero Heading Proper

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,11 +5,17 @@ import hero from "../assets/img/kuldeep-mishra.png";
 const Hero = () => {
   useEffect(() => {
     const options = {
-      strings: ["MERN Stack Developer,"],
-      typeSpeed: 100,
+      strings: ["MERN Stack Developer"],
+      typeSpeed: 200,
+      backSpeed: 100,
+      backDelay: 500,
+      startDelay: 1000,
       loop: true,
+      showCursor: false,
+      cursorChar: "|",
+      
     };
-    const typed = new Typed(".typed", options);
+    const typed = new Typed(".typed", options );
 
     return () => {
       typed.destroy();
@@ -37,7 +43,7 @@ const Hero = () => {
                 I am Kuldeep Mishra
               </h1>
               <p className="hero-subtitle">
-                <span className="typed text-white md:text-6xl text-4xl absolute top-[45%] md:top-[50%] left-0 w-full text-center font-serif" />
+              <h1 className="text-white md:text-6xl text-4xl absolute top-[45%] md:top-[50%] left-0 w-full text-center font-serif">  <span className="typed "></span>|</h1>
               </p>
             </div>
           </div>
